53e72533a09f19eccfa42fdbe525ae5575f8d0bd
Demarkus Matthews <dmanthony@Demarkuss-MacBook-Pro.local>
2019-07-11 13:39:55 -0700

    done with modals

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
+// grab slot machine
+const shakeMachine = document.querySelector('article');
 const wheel1 = document.querySelector('#wheel1');
 const wheel2 = document.querySelector('#wheel2');
 const wheel3 = document.querySelector('#wheel3');
-const wheel = document.querySelectorAll('.slot_holder');
-const play = document.querySelector('.btn_play');
-const btnMoney = document.querySelector('.btn_money');
+const lightBulb = document.querySelectorAll('.lightBulb');
+const wheel = document.querySelectorAll('.slotHolder');
 const money = document.querySelector('.money');
-const modal = document.querySelector('.simpleModal');
+
+// grab model
+const modalForm = document.getElementsByClassName('modal')[0];
+const modalInfo = document.getElementsByClassName('modal')[1];
+const modalNoMoney = document.getElementsByClassName('modal')[2];
+
+// grab button
+const playBtn = document.querySelector('.playBtn');
+const moneyBtn = document.querySelector('.moneyBtn');
 const modalBtn = document.querySelector('.modalBtn');
 const closeBtn = document.querySelector('.closeBtn');
-const submit = document.querySelector('.submit');
+const submitBtn = document.querySelector('.submitBtn');
 const input = document.querySelectorAll('input');
+const okayBtn = document.querySelector('.okayBtn');
+const addMoreMoneyBtn = document.querySelector('.addMoreMoneyBtn');
+
+// grab audio
 const handle = new Audio('audio/payout2.wav');
 const payOut = new Audio('audio/slot2.wav');
+
+// grab money
+const addMoney = document.querySelectorAll('.addMoney')[0];
+const addMoney1 = document.querySelectorAll('.addMoney')[1];
+
+// slot images
 const fruit = ['images/apple.png', 'images/banana.png', 'images/cherry.png', 'images/grape.png', 'images/orange.png', 'images/bar.png', 'images/watermelon.png', 'images/lemon.png'];
-play.disabled = true;
-submit.disabled = true;
+
+// button disabled
+playBtn.disabled = true;
 
 //  slot machine object
-
 const slotMachine = {
     money: 0,
     credit: 0,
@@ -64,40 +83,82 @@ const slotMachine = {
         else {
             money.textContent =  `$${this.money -= 2}`
         }
+    },
+
+    checkInput() {
+        const password = document.querySelectorAll('.password')[0].value;
+        const passwordRepeat = document.querySelectorAll('.password')[1].value;
+        while(password.length != passwordRepeat.length) {
+            submit.disabled = true;
+        }
+        
+    },
+
+    showNoMoney() {
+        modalNoMoney.style.display = 'block'; 
+    },
+
+    addMoneyForm() {
+       money.textContent = `$${this.money += +addMoney.value}`
+    },
+
+    addMoneyAgain() {
+        money.textContent = `$${this.money += +addMoney1.value}`
+        modalNoMoney.style.display = 'block';
     }
 }
 
 // functions
-
-const closeModal = () => {
-    modal.style.display = 'none';
-}
 const clickOutSide = (e) => {
-    if (e.target == modal) {
-    modal.style.display = 'none';
+    if (e.target == modalForm) {
+    modalForm.style.display = 'none';
     }
 }
 
-// const checkFields = () => {
-//     for(let i = 0; i < input.length; i++) {
-//         if(input[i].value === '') {
-//             submit.disable = true;
-
-//         } else {
-//             submit.disable = false;
-//         }
-//         console.log(input[i]);
-//     }
-    
-// }
-// checkFields();
-
 // event handlers
-
 window.addEventListener('click', clickOutSide)
 
-btnMoney.addEventListener('click', (e) => {
-    play.disabled = false;
+// submit form
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const password = document.querySelectorAll('.password')[0].value;
+        const passwordRepeat = document.querySelectorAll('.password')[1].value;
+        if(password.length !== passwordRepeat.length) {
+            submit.disabled = true;
+        }
+    slotMachine.addMoneyForm(); 
+    modalForm.style.display = 'none';   
+})
+
+// play game
+playBtn.addEventListener('click', () => {
+    if(slotMachine.money === 0) {
+        return slotMachine.showNoMoney();
+    }
+    slotMachine.play();
+    handle.play();
+    shakeMachine.classList.add('shakeMachine');
+    lightBulb.forEach(l => l.classList.add('flashLight'))
+    setTimeout(() => {
+        shakeMachine.classList.remove('shakeMachine');
+        lightBulb.forEach(l => l.classList.remove('flashLight'))
+    }, 8000)
+})
+
+// display INSTRUCTIONS
+window.onload = function() {
+    modalInfo.style.display = 'block';
+};
+
+// display FORM
+okayBtn.addEventListener('click', () => {
+    modalInfo.style.display = 'none';
+    modalForm.style.display = 'block';
+})
+
+// chose play OPTIONS
+moneyBtn.addEventListener('click', (e) => {
+    playBtn.disabled = false;
     const button = e.target.textContent;
     console.log(button);
     if (button == '20') {
@@ -111,12 +172,8 @@ btnMoney.addEventListener('click', (e) => {
     return slotMachine.credit
 })
 
-play.addEventListener('click', () => {
-    slotMachine.play();
-    handle.play();
-
+// add money again when out of money
+addMoreMoneyBtn.addEventListener('click', () => {
+    slotMachine.addMoneyAgain();
+    modalNoMoney.style.display = 'none';
 })
-
-window.onload = function() {
-    modal.style.display = 'block';
-};
